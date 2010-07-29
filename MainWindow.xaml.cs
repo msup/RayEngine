19e7800638c04dcc03f972d954529ed5ba5bbb08
Marek Suplata <marek.suplata@gmail.com>
2010-07-29 14:01:52 +0200

    RibbonWindow altered with Window. Problem with window related button fixed.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,33 +1,34 @@
-﻿using System.Windows;
+﻿using System.IO;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
-using System.IO;
+using AvalonDock;
 using Data;
 using WpfOpenTK.Managers;
-using Fluent;
-using AvalonDock;
 
 namespace WpfOpenTK
 {
 	/// <summary>
 	/// Interaction logic for MainWindow.xaml
 	/// </summary>
-	public partial class MainWindow : RibbonWindow
+	public partial class MainWindow : Window
 	{
 		#region private fields - managers
+
 		private RenderWindowManager rwm = null;
 		private DatasetManager dsm = null;
 		private LoggingManager lm = null;
 		private WindowManager wm = null;
 		private PluginManager pm = null;
+
 		#endregion
 
-		#region Constructors 
+		#region Constructors
 
 		public MainWindow()
 		{
 			InitializeComponent();
-			
+
 			lm = new LoggingManager();
 
 			wm  = new WindowManager( RibbonBar, StatusBar, DockWindow, DockingWindowsManager );
@@ -47,17 +48,26 @@ namespace WpfOpenTK
 
 		public DockPanel DockWindow
 		{
-			get { return ( new DockPanel() ); }
+			get
+			{
+				return ( new DockPanel() );
+			}
 		}
 
 		public RibbonBarPanel RibbonBar
 		{
-			get { return ( new RibbonBarPanel() ); }
+			get
+			{
+				return ( new RibbonBarPanel() );
+			}
 		}
 
 		public StatusBar StatusBar
 		{
-			get { return statusBar; }
+			get
+			{
+				return statusBar;
+			}
 		}
 
 		//public TabControl TabControl
@@ -67,7 +77,10 @@ namespace WpfOpenTK
 
 		public DockingManager DockingWindowsManager
 		{
-			get { return dockingManager; }
+			get
+			{
+				return dockingManager;
+			}
 		}
 
 		#endregion Properties
